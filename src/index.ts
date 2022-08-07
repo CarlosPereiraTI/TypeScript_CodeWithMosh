f8419aa87314976ca88d97686dfe846ef64a9aee
DESKTOP-JQAIFQV\User <carlospereira.ti@gmail.com>
2022-08-07 15:26:28 -0300

    first lines and environment configuration

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/*
+// console.log('Hello World');
+
+let age: number = 20;
+
+// unable to set another type to one already defined
+// age = 'a;'
+
+if (age < 50) {
+    age += 10;
+}
+
+console.log(age);
+*/
+
